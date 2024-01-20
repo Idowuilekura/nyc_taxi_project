d3c1e0fe3179614092b7ecbfe625d2c2d17168be
idowuilekura <ilekuraidowu@gmail.com>
2024-01-20 22:26:19 +0000

    major workflow

--- docker_sql/git_hub_workflow.sh
+++ docker_sql/git_hub_workflow.sh
@@ -26,19 +26,19 @@ fi
 echo 'version is' $VERSION
 # split the current version
 
-CURRENT_VERSION_PARTS=(${ CURRENT_VERSION//./ })
+CURRENT_VERSION_PARTS=(${CURRENT_VERSION//./})
 VNUM1=${CURRENT_VERSION_PARTS[0]}
 VNUM2=${CURRENT_VERSION_PARTS[1]}
 VNUM3=${CURRENT_VERSION_PARTS[2]}
 
-if [[ $VERSION=="MAJOR" ]]
+if [[ $VERSION == "MAJOR" ]]
     then 
     VNUM_NUMERICAL_PART="${VNUM1#v}"
     VNUM1="v$(($VNUM_NUMERICAL_PART+1))"
-elif [ [ $VERSION=="MINOR" ] ]
+elif [[ $VERSION == "MINOR" ]]
     then 
     VNUM2=$(($VUM2+1))
-elif [[ $VERSION=="PATCH "]]
+elif [[ $VERSION == "PATCH "]]
     then 
     VNUM3=$(($VNUM3+1))
 else 
@@ -55,7 +55,7 @@ GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 #only tag if no tag already
-if [-z "$NEEDS_TAG"]; then
+if [-z "$NEEDS_TAG" ]; then
     echo "Tagged with $NEW_TAG"
     git tag $NEW_TAG
     git push --tags
