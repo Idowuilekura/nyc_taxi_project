aeb6e2291e2b617b60da839fbb255ce9a3559c13
idowuilekura <ilekuraidowu@gmail.com>
2024-01-20 22:45:03 +0000

    major update

--- docker_sql/git_hub_workflow.sh
+++ docker_sql/git_hub_workflow.sh
@@ -57,7 +57,7 @@ GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 #only tag if no tag already
-if [-z "$NEEDS_TAG" ]; then
+if [ -z "$NEEDS_TAG" ]; then
     echo "Tagged with $NEW_TAG"
     git tag $NEW_TAG
     git push --tags
