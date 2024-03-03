a6916f39087a7e07d475b7d4ada3c83425200470
idowuilekura <ilekuraidowu@gmail.com>
2024-03-03 18:27:28 +0000

    added the files

--- docker_sql/etl_script/__init__.py
+++ docker_sql/etl_script/__init__.py
@@ -1,12 +1,12 @@
-# from .etl_script import ny_taxi_extract
-# from .etl_script import ny_taxi_transform
-# from .etl_script import ny_taxi_load_sql
-# from .etl_script import ny_run_etl_pipeline
+from .etl_script import ny_taxi_extract
+from .etl_script import ny_taxi_transform
+from .etl_script import ny_taxi_load_sql
+from .etl_script import ny_run_etl_pipeline
 
 # from . import etl_script
 
-__all__ = [
-    'ny_taxi_extract',
-    'ny_taxi_transform',
-    'ny_taxi_load_sql',
-    'ny_run_etl_pipeline']
+# __all__ = [
+#     'ny_taxi_extract',
+#     'ny_taxi_transform',
+#     'ny_taxi_load_sql',
+#     'ny_run_etl_pipeline']
